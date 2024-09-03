404dc8b774f0645df6b311ca5f7912727508f5cb
Miguel Angel Nivia O <85419069+Miguel-Angel-Nivia@users.noreply.github.com>
2024-09-02 20:45:10 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,54 +1,8 @@
-import os, time, textwrap
 from openal import *
+import os, time, textwrap, threading
 
 # Code Functionality
 # By: Miguel Angel Nivia Y Daniel Vasquez
-
-# List of Sounds routes.
-# Sonido de inicio : Sounds/sonidoEmpezar.wav
-# Sonido Musica Menu : Sounds/sonidoInicioMenu.wav
-# Sonido de viento : Sounds/sonidoViento.wav
-# Sonido de gotas de agua : Sounds/sonidoGotasCueva.wav
-# Sonido Seleccion Usuario : Sounds/Soundseleccion.wav
-# Sonido Herida Frederic : Sounds/sonidoHeridaFrederic.wav
-# Sonido Duda Frederic : Sounds/sonidoDudaFrederic.wav
-# Sonido Pasos Humano : Sounds/sonidoPasosHumano.wav
-# Sonido Fogata : Sounds/sonidoFogata.wav
-# Sonido Crafteo : Sounds/sonidoCrafteo.wav
-# Sonido Baba : Sounds/sonidoBaba.wav
-# Sonido Rocas Cayendo : Sounds/sonidoRocasCayendo.wav
-# Sonido Grito Mujer : Sounds/sonidoMujerGritando.wav
-# Sonido Rugido Monstruo : Sounds/sonidoMonstruoRugido.wav
-# Sonido Correr Humano : Sounds/sonidoCorrerHumano.wav
-# Sonido Brillo : Sounds/sonidoBrillo.wav
-# Sonido Armadura : Sounds/sonidoArmadura.wav
-# Sonido Murcielagos : Sounds/sonidoMurcielagos.wav
-# Sonido Fuego Incrementa : Sounds/sonidoFuegoIncremento.wav
-# Sonido Bar : Sounds/sonidoBar.wav
-# Sonido Rio : Sounds/sonidoRio.wav
-# Sonido Recoger : Sounds/sonidoRecoger.wav
-# Sonido Espada : Sounds/sonidoEspada.wav
-# Sonido Suspiro : Sounds/Soundsuspiro.wav
-# Sonido Risa Frederic : Sounds/sonidoRisaFrederic.wav
-# Sonido Araña : Sounds/sonidoAraña.wav
-# Sonido Ataque Araña : Sounds/sonidoAtaqueAraña.wav
-# Sonido Dolor Frederic : Sounds/sonidoDolorHombre.wav
-# Sonido Splash Agua : Sounds/SoundsplashAgua.wav
-# Sonido Pasos Jefe : Sounds/sonidoPasosJefe.wav
-# Sonido Ayuda Hombre : Sounds/sonidoAyudaM.wav
-# Sonido Toser Hombre : Sounds/sonidoToserM.wav
-# Sonido Atras Tuyo Jefe : Sounds/sonidoAtrasTuyo.wav
-# Sonido Muerte Aplastado : Sounds/sonidoAplastado.wav
-# Sonido Grito Muerte : Sounds/sonidoGritoMuerte.wav
-# Sonido Caida : Sounds/sonidoCaida.wav
-# Sonido Beso : Sounds/sonidoBeso.wav
-# Sonido Botella : Sounds/sonidoBotella.wav
-# Sonido Hechizo Healing : Sounds/sonidoHealing.wav
-# Sonido Hechizo Enchanter : Sounds/sonidoEnchanter.wav
-# Sonido Risa Maniatica : Sounds/sonidoManiatico.wav
-# Sonido Victoria : Sounds/sonidoVictoria.wav
-# Sonido Neutral : Sounds/sonidoNeutral.wav
-# Sonido Derrota : Sounds/sonidoDerrota.wav
 
 def obtainingSound():
     """
@@ -66,58 +20,131 @@ def obtainingSound():
         sounds[nombre] = sound
     return sounds
 
-def playSound(sounds, name, volume, spatialPos, spaceVel, speed, time, loop):
+
+def playSoundEspecialThreads(sound, playerPos, soundPos, initialVol, maxDistance):
+    """
+    Adjusts the sound volume based on the distance between the player and the target.
+    
+    Arguments:
+        sound (str): Target sound.
+        playerPos: The current position of the player (x, y, z).
+        soundPos: The position of the sound (x, y, z).
+        initial_volume: The initial volume of the sound.
+        max_distance: The maximum distance at which the sound should be audible.
+    """
+    sound.set_gain(initialVol)
+    sound.set_position(soundPos)
+    sound.play()
+    flag = True
+    while flag:
+        # Calculate the distance between the player and the campfire
+        distance = ((playerPos[0] - soundPos[0]) ** 2 + (playerPos[1] - soundPos[1]) ** 2 + (playerPos[2] - soundPos[2]) ** 2) ** 0.5
+        # Calculate the new volume based on the distance
+        if distance > maxDistance:
+            volume = 0
+        else:
+            volume = initialVol * (1 - (distance / maxDistance))
+        # Ajusta el volumen y la posición del sonido
+        sound.set_gain(volume)
+        sound.set_position(soundPos)
+        time.sleep(0.1)
+
+def playSoundThreads(sound, volume, speed, spatialPos, spaceVel, loop):
     """
     # Function to play Sounds, where you can modify various characteristics of it.
 
     Arguments:
-        name (str): Name of the sound file to play.
+        sound (str): Chosen sound.
         volume (float): Numerical value of sound volume where 1.0 is the standard.
+        speed (float): Speed of sound reproduction.
         spatialPos (list(float)): List of integers of size 3 containing the "x", "y" and "z" positions of the sound spatially.
                                 where x = 1 is right and -1 is left,
                                 where y = 1 is up and -1 is down and
                                 where z = 1 near (forward) and -1 far (back).
         spaceVel (list(float)): List of integers of size 3 containing the "x", "y", and "z" velocities of sound.
-        velocidad (float): speed of sound reproduction.
-        tiempo (float): execution time before sound is played.
         loop (bool): Flag indicating whether the sound plays infinitely or not.
+    """
+    # Sound settings.
+    sound.set_gain(volume)
+    sound.set_pitch(speed)
+    sound.set_position((spatialPos[0], spatialPos[1], spatialPos[2]))
+    sound.set_velocity((spaceVel[0], spaceVel[1], spaceVel[2]))
+    if loop:
+        sound.set_looping(True)
+    sound.play()
+    # Keep the sound playing in loop
+    while loop and sound.get_state() == AL_PLAYING:
+        time.sleep(0.1)
+
+
+def playSound(sounds, name, volume, speed, spatialPos, spaceVel, timeToWait, loop, sel):
+    """
+    Function to play Sounds concurrently using threads, where you can modify various characteristics of the sound.
+    Arguments:
+        The same ones that appear in "playSoundThreads" except this one:
+        sounds (dic): Sound dictionary.
+        name (str): Name of the sound file to play.
+        timeToWait (float): Execution time of the sound.
     """
     if name not in sounds:
         print(f"El sonido '{name}' no se encontró.")
-        return
-
+        return 1
+    
     # Load and play the sound file.
     sound = sounds[name]
-    # Sound settings.
-    sound.set_gain(volume)
-    sound.set_position((spatialPos[0], spatialPos[1], spatialPos[2]))
-    sound.set_velocity((spaceVel[0], spaceVel[1], spaceVel[2]))
-    sound.set_pitch(speed)
-    if time != 0:
-        time.sleep(time)
-    if loop == True:
-        sound.set_looping(True)
-        # Keep the sound playing in loop until the user presses Enter
-        sound.play()
-        try:
-            input("\n                         Presiona enter para continuar...                            \n\n")
-        finally:
-            sound.stop()
-    else:
-        sound.play()
-        # Keep the sound playing.
-        while sound.get_state() == AL_PLAYING:
-            pass
-    return 0
+    if sel == "static":
+        # Setting up and playing sound in a separate threads
+        thread = threading.Thread(target = playSoundThreads, args = (sound, volume, speed, spatialPos, spaceVel, loop))
+        thread.start()
+        if timeToWait != 0:
+            time.sleep(timeToWait)
+    elif sel == "move":
+        # Create a thread to adjust the sound based on distance
+        thread = threading.Thread(target = playSoundEspecialThreads, args = (sound, [0, 0, 0], spatialPos, volume, 12))
+        thread.start()
+
+    return thread
 
 def printText(text, clean):
     # Function to display text in cmd with a width of 70.
     line = textwrap.wrap(text, width = 80)
     for i in line:
         print(i)
-    input("\nPresiona enter para continuar...\n")
+    entrada = None
+    while entrada != "":
+        entrada = input("\nPresiona enter para continuar...\n")
     if clean == 1:
         os.system('cls')
+
+    return 0
+
+def playerInteraction(sounds, info, text, thread, sound, number):
+    """
+    Function to execute the player selection for a decision/Interaction.
+    Arguments:
+        sounds (dic): sound dictionary.
+        info (str): Information on action to be taken.
+        text (str): Text to print.
+        thread (thread): Thread to be executed.
+        sound (str): Name of the sound.
+        number (int): Number to know if you want to clean the console or not.
+    """
+    if info == "selection":
+        decision = None
+        selectionThread = playSound(sounds, "sonidoSeleccion.wav", 0.7, 2, [0, 0, 0], [0, 0, 0], 0, False, "static")
+        if selectionThread.is_alive():
+            try:
+                decision = input(":")
+            finally:
+                sounds["sonidoSeleccion.wav"].stop() and selectionThread.join()
+        return decision
+    
+    elif info == "continue":
+        if thread.is_alive():
+            try:
+                printText(text, number)
+            finally:
+                sounds[sound].stop() and thread.join()
     return 0
 
 def loadingScreen():
@@ -153,6 +180,5 @@ def loadingScreen():
     print("|  |  |  _  |  |_   _|  |  |     | __  |  _  |  |    \|   __|  |   __|   __|    \|  _  |")
     print("|  |__|     |    | | |  |  | | | | __ -|     |  |  |  |   __|  |__   |   __|  |  |     |")
     print("|_____|__|__|    |_| |_____|_|_|_|_____|__|__|  |____/|_____|  |_____|_____|____/|__|__|")
-    print("                                                                                        ")
-    print("                                  Hecho Por: Miguel Angel Nivia Y Daniel Vasquez Murillo")
+    print("\n                                Hecho Por: Miguel Angel Nivia Y Daniel Vasquez Murillo")
     return 0
